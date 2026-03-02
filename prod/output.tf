ee667360480099ceba1f479bdcbee4424827be65
Shivam Sharma <shivam.sharma@rtctek.global>
2026-03-02 13:55:33 +0530

    dev

--- prod/output.tf
+++ prod/output.tf
@@ -3,6 +3,6 @@ output "bastion_public_ip" {
   value       = module.bastion.public_ip
 }
 
-# output "alb_dns_name" {
-#   value = module.alb.dns_name
-# }
+output "alb_dns_name" {
+  value = module.alb.dns_name
+}
