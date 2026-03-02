a4e5e24e96969c49cb9bec40e607eb0ba62db3fb
Shivam Sharma <118384998+Shivamssharma03@users.noreply.github.com>
2026-03-02 14:13:51 +0530

    Update backend.tf

--- prod/backend.tf
+++ prod/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "shivamrtcbackend"
-    key            = "rtcinfrastructure/prod/terraform.tfstate"
+    key            = "rtcinfrastructure/prod/terraform.tfstatee"
     region         = "us-east-1"
     dynamodb_table = "rtcbackendtable"
 
